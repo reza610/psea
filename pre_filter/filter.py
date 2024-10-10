b6f1ce4d27ed9a05c6641ffd747a77bf161e28ce
ozeroff <ozeroff@colorado.edu>
2024-10-10 12:35:46 -0600

    error handling added

--- pre_filter/filter.py
+++ pre_filter/filter.py
@@ -40,76 +40,100 @@ def run_filtering(patient_comorbid_threshold,
     - include_binary_attribute_file = Name/location of file to contain filtered list of cormorbidities.
     
     """
+    ###############################
+    ## comorbidity csv filtering ##
+    ###############################
     
-    ##comorbidity csv filtering##
-    
-    # get working dir and read in the CSV file
-    #sample_name = 'Patient'
-    # binary_attribute_file = '../testdata/comorbid_file.csv'
-    file_path = binary_attribute_file
-    df = pd.read_csv(file_path, index_col=0)
-    
-    # number samples
-    total_samples = df.shape[0]
-    
-    # Filter out patients without comorbidities
-    # patient_comorbid_threshold = 1
-    filtered_df = df[df.drop(columns=[sample_name]).sum(axis=1) >= patient_comorbid_threshold]
-    
-    # drop sample_names column
-    df_noname = filtered_df.drop(columns=[sample_name])
-    
-    # Calculate percent occurence of comorbidities
-    n_comorbid = df_noname.sum(axis=0).to_frame()
-    n_comorbid.columns =["n"]
-    n_comorbid["binary_attribute"] = n_comorbid.index
-    n_comorbid["percent"] =  n_comorbid["n"]/total_samples
+    try:
+        # get working dir and read in the CSV file
+        file_path = binary_attribute_file
+        if not os.path.isfile(binary_attribute_file):
+            raise FileNotFoundError(f"Binary attribute file '{binary_attribute_file}' not found.")
+        df = pd.read_csv(file_path, index_col=0)
+        # check that the sample_name column actually exists
+        if sample_name not in df.columns:
+            raise ValueError(f"Sample column labeled '{sample_name}' not found in the binary attribute file.")
 
-    # Filter based on user input min and max comorbid occurence
-    #max_comorbids_percent = 0.9
-    #min_comorbids_percent = 0.1
-    n_comorbid = n_comorbid[n_comorbid["percent"]<max_comorbids_percent]
-    n_comorbid = n_comorbid[n_comorbid["percent"]>min_comorbids_percent]
-    print(n_comorbid)
+        # get the number samples
+        total_samples = df.shape[0]
+
+        # Filter out patients without comorbidities
+        if patient_comorbid_threshold < 0:
+            raise ValueError("Patient comorbid threshold must be a positive number.")
+        filtered_df = df[df.drop(columns=[sample_name]).sum(axis=1) >= patient_comorbid_threshold]
+
+        # drop sample_names column
+        df_noname = filtered_df.drop(columns=[sample_name])
+
+        # Calculate percent occurence of comorbidities
+        n_comorbid = df_noname.sum(axis=0).to_frame()
+        n_comorbid.columns =["n"]
+        n_comorbid["binary_attribute"] = n_comorbid.index
+        n_comorbid["percent"] =  n_comorbid["n"]/total_samples
+
+        # Filter based on user input min and max comorbid occurence
+        if not (0 <= min_comorbids_percent <= 1 and 0 <= max_comorbids_percent <= 1):
+            raise ValueError("Comorbid percentage thresholds must be between 0 and 1.")
+        n_comorbid = n_comorbid[n_comorbid["percent"]<max_comorbids_percent]
+        n_comorbid = n_comorbid[n_comorbid["percent"]>min_comorbids_percent]
+        print(n_comorbid)
+
+        # name the output file and save it based on include_binary_attribute_file argument input
+        if not os.access(os.path.dirname(include_binary_attribute_file), os.W_OK):
+            raise PermissionError(f"Cannot write to directory '{include_binary_attribute_file}'. Did you designate the ouput directoty and filename correctly?")
+        outdir = include_binary_attribute_file
+        n_comorbid[["binary_attribute"]].to_csv(outdir, header=False, index=False)
+        print("COMORBID FILE SAVED")
+        
+    except Exception as e:
+        print(f"Error occurred during comorbidity filtering: {e}")
     
-    # name the output file and save it based on include_binary_attribute_file argument input
-    #include_binary_attribute_file = '../testdata/include_binary_attribute_long.csv'
-    outdir = include_binary_attribute_file
-    n_comorbid[["binary_attribute"]].to_csv(outdir, header=False, index=False)
-    print("COMORBID FILE SAVED")
+    ###################################
+    ## gene expression csv filtering ##
+    ###################################
     
-    ##gene expression csv filtering##
+    try:
+        # get working dir and read in the CSV file for gene expression data
+        file_path = values_file
+        if not os.path.isfile(values_file):
+            raise FileNotFoundError(f"Values file '{values_file}' not found.")
+        df = pd.read_csv(file_path, index_col=0)
+        
+        # Validate if the sample_name column exists
+        if sample_name not in df.columns:
+            raise ValueError(f"Sample column '{sample_name}' not found in the values file.")
+        # drop samples column
+        expression_df_nosamples = df.drop(columns=[sample_name])
+
+        # Remove individuals (rows) where all gene expression values are below the threshold
+        individual_mask = expression_df_nosamples.apply(lambda row: (row >= individual_expression_threshold).any(), axis=1)
+        expression_df_nosamples = expression_df_nosamples[individual_mask]
+
+        # get mean expression, turn into pandas dataframe
+        # min_mean_expression = 0.1
+        meansdf=expression_df_nosamples.mean(axis=0).to_frame()
+        meansdf.columns = ["mean_value"]
+        meansdf["valuename"]= meansdf.index
+        
+        # Validate if min_mean_expression is a valid threshold
+        if min_mean_expression < 0:
+            raise ValueError("Minimum mean expression threshold must be non-negative.")
+
+        # filter based on mean expression
+        meansdf_include = meansdf[meansdf["mean_value"]>min_mean_expression]
+        print(meansdf_include)
+
+        # save output file based on include_values_file argument input
+        if not os.access(os.path.dirname(include_values_file), os.W_OK):
+            raise PermissionError(f"Cannot write to directory '{include_values_file}'. Did you designate the ouput directoty and filename correctly?")
+       
+        outdir = include_values_file
+        meansdf_include[["valuename"]].to_csv(outdir, header=False, index=False)
+        print("EXPRESSION FILE SAVED")
     
-    # get working dir and read in the CSV file for gene expression data
-    # values_file = '../testdata/value_expression.csv'
-    file_path = values_file
-    df = pd.read_csv(file_path, index_col=0)
-    
-    # drop samples column
-    expression_df_nosamples = df.drop(columns=[sample_name])
-    
-    # Remove individuals (rows) where all gene expression values are below the threshold
-    #individual_expression_threshold = 10
-    individual_mask = expression_df_nosamples.apply(lambda row: (row >= individual_expression_threshold).any(), axis=1)
-    # Filter the DataFrame using the individual_mask
-    expression_df_nosamples = expression_df_nosamples[individual_mask]
-    
-    # get mean expression, turn into pandas dataframe
-    # min_mean_expression = 0.1
-    meansdf=expression_df_nosamples.mean(axis=0).to_frame()
-    meansdf.columns = ["mean_value"]
-    meansdf["valuename"]= meansdf.index
-    
-    # filter based on mean expression
-    meansdf_include = meansdf[meansdf["mean_value"]>min_mean_expression]
-    print(meansdf_include)
-    
-    # save output file based on include_values_file argument input
-    #include_values_file = "../testdata/include_values_long.csv"
-    outdir = include_values_file
-    meansdf_include[["valuename"]].to_csv(outdir, header=False, index=False)
-    print("EXPRESSION FILE SAVED")
-    
+    except Exception as e:
+        print(f"Error occurred during gene expression filtering: {e}")
+        
     ####Some ideas going forward:
-    # Need to add more error handling
+    # Need to test all of the error handling
     
